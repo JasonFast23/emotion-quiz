ccce2e0e074aef955fcd12199e3ce3fda22e50bf
jae_sunn <jasonfast23@gmail.com>
2023-01-12 12:41:48 -0800

    Initial Commit

---
+++ screen after screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  screen after screen
+//
+//  Created by Jason Park on 1/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
